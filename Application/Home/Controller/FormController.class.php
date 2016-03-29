725b94c412fb5dd800143447c4ce891fa7894d37
SunSkyZhao <dingchao438@qq.com>
2016-03-29 15:29:02 +0800

    start a php app today curd

--- Application/Home/Controller/FormController.class.php
+++ Application/Home/Controller/FormController.class.php
@@ -120,7 +120,8 @@ class FormController extends Controller{
     * dingChao
     * 2016/3/29
     */
-    public function select(){
+    public function select()
+    {
         $user = M('form');
         //字符串查询开始
         //二维数组
@@ -133,24 +134,68 @@ class FormController extends Controller{
         $condition['title'] = '123';
         $condition['id'] = '123';
         $condition['_logic'] = 'OR';//逻辑语句，等同name=123 or title=123
-        $condition['id']  = array('eq',100);//表达式查询id=100
-        $condition['name'] = array('like','thinkphp%');//模糊查询
-        $condition['id']  = array('between',array('1','8'));//等效id BETWEEN 1 AND 8
+        $condition['id'] = array('eq', 100);//表达式查询id=100
+        $condition['name'] = array('like', 'thinkphp%');//模糊查询
+        $condition['id'] = array('between', array('1', '8'));//等效id BETWEEN 1 AND 8
         $data = $user->where($condition)->find();
         //数组查询结束
+        //快捷查询开始
         $User = M("User"); // 实例化User对象
         $map['name|title'] = 'thinkphp';
         // 把查询条件传入查询方法
+        $map['status&score&title'] = array('1', array('gt', '0'), 'thinkphp', '_multi' => true);
+        //'_multi'=>true必须加在数组的最后，表示当前是多条件匹配
+        //等价于 status= 1 AND score >0 AND  title = 'thinkphp'
+        //快捷查询方式中“|”和“&”不能同时使用
+        //区间查询
+        $map['id'] = array(array('gt', 3), array('lt', 10), 'or');
+        //等价于(`id` > 3) OR (`id` < 10)
         $User->where($map)->select();//name= 'thinkphp' OR title = 'thinkphp'
-        //快捷查询开始
-
         //快捷查询结束
-        if($data){
+        //复合查询
+        $where['name'] = array('like', '%thinkphp%');
+        $where['title'] = array('like', '%thinkphp%');
+        $where['_logic'] = 'or';
+        $map['_complex'] = $where;//插入的复合查询条件数组
+        $map['id'] = array('gt', 1);
+        //等价于
+        //( id > 1) AND ( ( name like '%thinkphp%') OR ( title like '%thinkphp%') )
+        //复合查询结束
+        //统计查询开始
+        $User = M("User"); // 实例化User对象
+        // 获取用户数：
+        $userCount = $User->count();
+        // 或者根据字段统计：
+        $userCount = $User->count("id");
+        // 获取用户的最大积分：
+        $maxScore = $User->max('score');
+        //统计查询结束
+        //分页，page第一个参数是页数，第二个参数是每页的数据数目
+        $this->page(5, 25)->select();
+        if ($data) {
             $this->success('成功');
             die();
-        }else{
+        } else {
             $this->error('失败');
             die();
         }
+    }
+    /*
+    * 获取数据
+    * dingChao
+    * 2016/3/29
+    */
+    public function data(){
+        echo I('get.id',0); // 如果不存在$_GET['id'] 则返回0
+        echo I('get.name',''); // 如果不存在$_GET['name'] 则返回空字符串
+        // 采用htmlspecialchars方法对$_GET['name'] 进行过滤，如果不存在则返回空字符串
+        echo I('get.name','','htmlspecialchars');//最后一个参数是过滤方法
+        // 获取整个$_GET 数组
+        //I方法可以过滤绝大多数参数
+        I('get.');
+        I('post.name','','htmlspecialchars'); // 采用htmlspecialchars方法对$_POST['name'] 进行过滤，如果不存在则返回空字符串
+        I('session.user_id',0); // 获取$_SESSION['user_id'] 如果不存在则默认为0
+        I('cookie.'); // 获取整个 $_COOKIE 数组
+        I('server.REQUEST_METHOD'); // 获取 $_SERVER['REQUEST_METHOD']
     }
 }
